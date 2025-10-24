4135854de1de9a3c5db638b6e0dba334b6b70359
Roman Lee ツ <git@romanlee17.com>
2025-10-24 14:51:51 +0300

    first api iteration

---
+++ Runtime/YandexMobileAds.cs
@@ -0,0 +1,254 @@
+using MirraGames.SDK.Common;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using YandexPlugin = global::YandexMobileAds;
+using YandexPluginBase = global::YandexMobileAds.Base;
+using Logger = MirraGames.SDK.Common.Logger;
+
+namespace MirraGames.SDK.YandexMobileAds {
+
+    [Provider(typeof(IAds))]
+    public class YandexMobileAds : CommonAds {
+
+        private readonly YandexMobileAds_Configuration configuration;
+
+        // Interstitial fields
+        private YandexPlugin.InterstitialAdLoader _interstitialLoader;
+        private YandexPlugin.Interstitial _interstitial;
+        private Action _currentInterstitialOnOpen;
+        private Action<bool> _currentInterstitialOnClose;
+
+        // Rewarded fields
+        private YandexPlugin.RewardedAdLoader _rewardedLoader;
+        private YandexPlugin.RewardedAd _rewarded;
+        private Action _currentRewardedOnOpen;
+        private Action<bool> _currentRewardedOnClose;
+        private bool _rewardGranted;
+
+        public YandexMobileAds(YandexMobileAds_Configuration configuration, IEventAggregator eventAggregator) : base(eventAggregator) {
+            this.configuration = configuration;
+        }
+
+        private string GetInterstitialAdUnitId() {
+#if UNITY_IOS
+            return configuration.InterstitialAdUnitIdIOS;
+#elif UNITY_ANDROID
+            return configuration.InterstitialAdUnitIdAndroid;
+#else
+            Logger.Warning(this, "Unsupported platform for Yandex Ads. Using Android Ad Unit ID by default.");
+            return configuration.InterstitialAdUnitIdAndroid;
+#endif
+        }
+
+        private string GetRewardedAdUnitId() {
+#if UNITY_IOS
+            return configuration.RewardedAdUnitIdIOS;
+#elif UNITY_ANDROID
+            return configuration.RewardedAdUnitIdAndroid;
+#else
+            Logger.Warning(this, "Unsupported platform for Yandex Ads. Using Android Ad Unit ID by default.");
+            return configuration.RewardedAdUnitIdAndroid;
+#endif
+        }
+
+        // Banner is not supported in Yandex Mobile Ads SDK hence these methods just log warnings
+
+        protected override void InvokeBannerImpl() {
+            Logger.NotAvailableWarning(this, nameof(InvokeBannerImpl));
+        }
+
+        protected override void RefreshBannerImpl() {
+            Logger.NotAvailableWarning(this, nameof(RefreshBannerImpl));
+        }
+
+        protected override void DisableBannerImpl() {
+            Logger.NotAvailableWarning(this, nameof(DisableBannerImpl));
+        }
+
+        // Implement interstitial and rewarded ads methods using Yandex Mobile Ads SDK
+        // The contract should be simple, you call invoke and it shows the ad with onopen callback, and after ad is closed by user, it calls onclose
+        // Everything else should be handled inside this class, like ad ids just hardcode what is needed to start ad above in the fields and I'll refactor later
+
+        protected override void InvokeInterstitialImpl(Action onOpen = null, Action<bool> onClose = null) {
+            string adUnitId = GetInterstitialAdUnitId();
+            if (string.IsNullOrEmpty(adUnitId)) {
+                onClose?.Invoke(false);
+                return;
+            }
+
+            _currentInterstitialOnOpen = onOpen;
+            _currentInterstitialOnClose = onClose;
+
+            if (_interstitial != null) {
+                AttachInterstitialHandlers();
+                _interstitial.Show();
+            }
+            else {
+                if (_interstitialLoader == null) {
+                    _interstitialLoader = new YandexPlugin.InterstitialAdLoader();
+                    _interstitialLoader.OnAdLoaded += HandleInterstitialLoaded;
+                    _interstitialLoader.OnAdFailedToLoad += HandleInterstitialFailedToLoad;
+                }
+
+                var config = new YandexPluginBase.AdRequestConfiguration.Builder(adUnitId).Build();
+                try {
+                    _interstitialLoader.LoadAd(config);
+                }
+                catch (Exception ex) {
+                    Logger.CreateError(this, $"Failed to load interstitial ad configuration: {ex.Message}");
+                    _currentInterstitialOnClose?.Invoke(false);
+                    _currentInterstitialOnOpen = null;
+                    _currentInterstitialOnClose = null;
+                }
+            }
+        }
+
+        private void HandleInterstitialLoaded(object sender, YandexPluginBase.InterstitialAdLoadedEventArgs args) {
+            _interstitial = args.Interstitial;
+            AttachInterstitialHandlers();
+            _interstitial.Show();
+        }
+
+        private void HandleInterstitialFailedToLoad(object sender, YandexPluginBase.AdFailedToLoadEventArgs args) {
+            Logger.CreateError(this, $"Interstitial ad failed to load: {args.Message}");
+            _currentInterstitialOnClose?.Invoke(false);
+            _currentInterstitialOnOpen = null;
+            _currentInterstitialOnClose = null;
+        }
+
+        private void AttachInterstitialHandlers() {
+            if (_interstitial == null) return;
+
+            _interstitial.OnAdShown += HandleInterstitialShown;
+            _interstitial.OnAdDismissed += HandleInterstitialDismissed;
+            _interstitial.OnAdFailedToShow += HandleInterstitialFailedToShow;
+        }
+
+        private void HandleInterstitialShown(object sender, EventArgs e) {
+            _currentInterstitialOnOpen?.Invoke();
+        }
+
+        private void HandleInterstitialDismissed(object sender, EventArgs e) {
+            _currentInterstitialOnClose?.Invoke(true);
+            CleanupInterstitial();
+            _currentInterstitialOnOpen = null;
+            _currentInterstitialOnClose = null;
+        }
+
+        private void HandleInterstitialFailedToShow(object sender, YandexPluginBase.AdFailureEventArgs e) {
+            Logger.CreateError(this, $"Interstitial ad failed to show: {e.Message}");
+            _currentInterstitialOnClose?.Invoke(false);
+            CleanupInterstitial();
+            _currentInterstitialOnOpen = null;
+            _currentInterstitialOnClose = null;
+        }
+
+        private void CleanupInterstitial() {
+            if (_interstitial != null) {
+                _interstitial.OnAdShown -= HandleInterstitialShown;
+                _interstitial.OnAdDismissed -= HandleInterstitialDismissed;
+                _interstitial.OnAdFailedToShow -= HandleInterstitialFailedToShow;
+                _interstitial.Destroy();
+                _interstitial = null;
+            }
+        }
+
+        protected override void InvokeRewardedImpl(Action onOpen = null, Action<bool> onClose = null, string rewardTag = null) {
+            // rewardTag is ignored as per Yandex SDK; rewards are handled generically
+            string adUnitId = GetRewardedAdUnitId();
+            if (string.IsNullOrEmpty(adUnitId)) {
+                onClose?.Invoke(false);
+                return;
+            }
+
+            _currentRewardedOnOpen = onOpen;
+            _currentRewardedOnClose = onClose;
+            _rewardGranted = false;
+
+            if (_rewarded != null) {
+                AttachRewardedHandlers();
+                _rewarded.Show();
+            }
+            else {
+                if (_rewardedLoader == null) {
+                    _rewardedLoader = new YandexPlugin.RewardedAdLoader();
+                    _rewardedLoader.OnAdLoaded += HandleRewardedLoaded;
+                    _rewardedLoader.OnAdFailedToLoad += HandleRewardedFailedToLoad;
+                }
+
+                var config = new YandexPluginBase.AdRequestConfiguration.Builder(adUnitId).Build();
+                try {
+                    _rewardedLoader.LoadAd(config);
+                }
+                catch (Exception ex) {
+                    Logger.CreateError(this, $"Failed to load rewarded ad configuration: {ex.Message}");
+                    _currentRewardedOnClose?.Invoke(false);
+                    _currentRewardedOnOpen = null;
+                    _currentRewardedOnClose = null;
+                }
+            }
+        }
+
+        private void HandleRewardedLoaded(object sender, YandexPluginBase.RewardedAdLoadedEventArgs args) {
+            _rewarded = args.RewardedAd;
+            _rewardGranted = false;
+            AttachRewardedHandlers();
+            _rewarded.Show();
+        }
+
+        private void HandleRewardedFailedToLoad(object sender, YandexPluginBase.AdFailedToLoadEventArgs args) {
+            Logger.CreateError(this, $"Rewarded ad failed to load: {args.Message}");
+            _currentRewardedOnClose?.Invoke(false);
+            _currentRewardedOnOpen = null;
+            _currentRewardedOnClose = null;
+        }
+
+        private void AttachRewardedHandlers() {
+            if (_rewarded == null) return;
+
+            _rewarded.OnAdShown += HandleRewardedShown;
+            _rewarded.OnAdDismissed += HandleRewardedDismissed;
+            _rewarded.OnAdFailedToShow += HandleRewardedFailedToShow;
+            _rewarded.OnRewarded += HandleRewardedRewarded;
+        }
+
+        private void HandleRewardedShown(object sender, EventArgs e) {
+            _currentRewardedOnOpen?.Invoke();
+        }
+
+        private void HandleRewardedDismissed(object sender, EventArgs e) {
+            _currentRewardedOnClose?.Invoke(_rewardGranted);
+            CleanupRewarded();
+            _currentRewardedOnOpen = null;
+            _currentRewardedOnClose = null;
+        }
+
+        private void HandleRewardedRewarded(object sender, YandexPluginBase.Reward reward) {
+            _rewardGranted = true;
+            // Reward details (amount, type) are available in 'reward' but ignored per contract; bool indicates grant
+        }
+
+        private void HandleRewardedFailedToShow(object sender, YandexPluginBase.AdFailureEventArgs e) {
+            Logger.CreateError(this, $"Rewarded ad failed to show: {e.Message}");
+            _currentRewardedOnClose?.Invoke(false);
+            CleanupRewarded();
+            _currentRewardedOnOpen = null;
+            _currentRewardedOnClose = null;
+        }
+
+        private void CleanupRewarded() {
+            if (_rewarded != null) {
+                _rewarded.OnAdShown -= HandleRewardedShown;
+                _rewarded.OnAdDismissed -= HandleRewardedDismissed;
+                _rewarded.OnAdFailedToShow -= HandleRewardedFailedToShow;
+                _rewarded.OnRewarded -= HandleRewardedRewarded;
+                _rewarded.Destroy();
+                _rewarded = null;
+            }
+        }
+
+    }
+
+}
---
+++ Runtime/YandexMobileAds_Configuration.cs
@@ -0,0 +1,43 @@
+using MirraGames.SDK.Common;
+using UnityEngine;
+
+namespace MirraGames.SDK.YandexMobileAds {
+
+    [ProviderConfiguration(typeof(YandexMobileAds))]
+    public class YandexMobileAds_Configuration : PropertyGroup {
+
+        public override string Name => nameof(YandexMobileAds);
+
+        [field: SerializeField] public string InterstitialAdUnitIdAndroid { get; private set; } = "demo-interstitial-yandex";
+        [field: SerializeField] public string InterstitialAdUnitIdIOS { get; private set; } = "demo-interstitial-yandex";
+        [field: SerializeField] public string RewardedAdUnitIdAndroid { get; private set; } = "demo-rewarded-yandex";
+        [field: SerializeField] public string RewardedAdUnitIdIOS { get; private set; } = "demo-rewarded-yandex";
+
+        public override StringProperty[] GetStringProperties() {
+            return new StringProperty[] {
+                new(
+                    "Interstitial UID Android",
+                    getter: () => { return InterstitialAdUnitIdAndroid; },
+                    setter: (value) => { InterstitialAdUnitIdAndroid = value; }
+                ),
+                new(
+                    "Interstitial UID iOS",
+                    getter: () => { return InterstitialAdUnitIdIOS; },
+                    setter: (value) => { InterstitialAdUnitIdIOS = value; }
+                ),
+                new(
+                    "Rewarded UID Android",
+                    getter: () => { return RewardedAdUnitIdAndroid; },
+                    setter: (value) => { RewardedAdUnitIdAndroid = value; }
+                ),
+                new(
+                    "Rewarded UID iOS",
+                    getter: () => { return RewardedAdUnitIdIOS; },
+                    setter: (value) => { RewardedAdUnitIdIOS = value; }
+                )
+            };
+        }
+
+    }
+
+}
